ea3e8acd1af511106e2f9f8e9c94db6ed44cf903
Ashish <yashish393@gmail.com>
2024-02-06 12:58:49 +0530

    Javascript_Basics

---
+++ day30_Js_2_Basics/script.js
@@ -0,0 +1,38 @@
+// this is console log
+// console.log("Hello Gojo");
+
+// let number1 = 10;
+// let number2 = 20;
+// console.log(number1+number2);
+
+// let name = "Ashish"
+// console.log(name);
+// name is reserve keyword of javascript
+
+// let Name = "itachi uchiha"
+// console.log(Name);
+
+// let age = 18;
+
+// let name1 = "superman"
+// let name2 = "spiderman"
+// console.log(name1, "is belong from DC and",name2,"is belong from marvel");
+// console.log(`"${name1}" is belong from DC and "${name2}" is belong from marvel`);
+
+// const ayz = "Ashish"
+// ayz = "gourav"
+// console.log(ayz);
+
+// ============================operators====================
+
+// let number1 = 20
+// let number2 = 30
+// console.log(number1 + number2);
+// console.log(number1 * number2);
+// console.log(number1 % number2);
+// console.log(number1 + number2);
+// console.log(number1++, ++number1);
+
+const number = "20";
+console.log(number === 20);
+console.log(typeof number);
