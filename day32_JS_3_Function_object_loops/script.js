543582086ba5ea531eb53f5b046c7a7351b294c2
Ashish <yashish393@gmail.com>
2024-02-08 13:06:34 +0530

    Javascript_day3_Functions_Objects

---
+++ day32_JS_3_Function_object_loops/script.js
@@ -0,0 +1,55 @@
+// ==========================function=====================
+// normal function declaration
+// function print() {
+//   console.log("This is a function body");
+// }
+// print();
+
+// function with return type
+// function sbiRateInterest() {
+//     return 7.8
+// }
+// console.log(sbiRateInterest())
+
+// function with arguments
+// function sum(a,b) {
+//     return a+b;
+// }
+// let result = sum(1,2)
+// console.log(result)
+
+// normal arrow function declaration
+// const print = () => {
+//   console.log("This is arrow function")
+// }
+// print()
+
+// Arrow function with return value
+// const rateOfInterest = () => 7.9;
+// console.log(rateOfInterest());
+
+// Arrow function with arguments and return type
+// const sum = (a, b) => {
+//   return a + b;
+// };
+// console.log(sum(1, 2));
+
+// =========================object======================
+const laptop = {
+    model_name:"Hp probook 830 G8",
+    brand_name:"Hp",
+    ram:"10GB"
+}
+console.log("laptop object" ,laptop);
+
+const laptop1 = {...laptop,ram:"5GB"}
+console.log("laptop1 object",laptop1);
+
+// let acc_num = 45897979
+// const Account = (name,addhar,pan,dob) => {
+// console.log(`Your name is ${name}\nYour acoount number is ${acc_num}\n`);
+//     acc_num++;
+// }
+// Account("Ashish",915472257235,"BCPPY1343C","30/10/2001")
+// Account("Gourav",915472257235,"BCPPY1343C","30/10/2001")
+
