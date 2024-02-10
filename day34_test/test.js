da7b17028bc4fb2f09756eb88c8eadfa07240cf9
Ashish <yashish393@gmail.com>
2024-02-10 13:32:18 +0530

    test

---
+++ day34_test/test.js
@@ -0,0 +1,62 @@
+// reverse string
+// find vovels
+// function get full name with object
+// print employee information using back tick
+// pallandrome string
+
+// Reverse string
+// const reverseString = (name) => {
+//   let temp = []
+//   for (let i = name.length - 1; i >= 0; i--) {
+//     temp = temp+name[i]
+//   }
+//   console.log(temp);
+// };
+// reverseString("ashish")
+
+// find vovels
+// const Vovels = (input) => {
+//   let char = [];
+//   let count = 0;
+//   for (let i = 0; i < input.length; i++) {
+//     if (
+//       input[i] == "a" ||
+//       input[i] == "e" ||
+//       input[i] == "i" ||
+//       input[i] == "o" ||
+//       input[i] == "u"
+//     ) {
+//       char = char + input[i];
+//     }
+//   }
+//   console.log(char);
+// };
+// Vovels("ashish");
+
+// get full name
+// const getFullName = (name,surname) => {
+//     let person = {
+//         name:name,
+//         surname:surname
+//     }
+//     console.log(person.name+person.surname)
+// }
+// getFullName("Ashish","Yadav")
+
+// // get palindrome
+// const palindrome = (name) => {
+//   let temp = [];
+//   for (let i = name.length - 1; i >= 0; i--) {
+//     temp = temp + name[i];
+//   }
+//   if (temp == name) {
+//     console.log("pallandrome");
+//   } else {
+//     console.log("not pallandrome");
+//   }
+// };
+// palindrome("yadav");
+
+
+
+
