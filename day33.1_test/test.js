375e28c417334b24c646d6eecbd5e9557db69535
Ashish <yashish393@gmail.com>
2024-02-14 11:08:19 +0530

    javascript_arrays_methods

--- day33.1_test/test.js
+++ day33.1_test/test.js
@@ -56,7 +56,3 @@
 //   }
 // };
 // palindrome("yadav");
-
-
-
-
---
+++ day34_JS_5_Arrays/script.js
@@ -0,0 +1,45 @@
+// let heroes = [
+//   "superman",
+//   "spiderman",
+//   "batman",
+//   "hulk",
+//   120,
+//   203466.234,
+//   "shaktiman",
+// ];
+// let arrObj = [
+//   { name: "gourav", place: "dharamsala", age: "nabalik" },
+//   { name: "ashish", city: "indore", age: 22 },
+//   { name: "ram", city: "ayodhya", age: "infinity" },
+//   { name: "hitesh", gav: "dholakpur", age: "2" },
+// ];
+// console.log(arrObj[0]);
+
+// for (const key in arrObj) {
+//   if (Object.hasOwnProperty.call(arrObj, key)) {
+//     const element = arrObj[key];
+//     console.log(element.name);
+//   }
+// }
+
+// shift and pop is brother
+// unshift and push is brother
+
+let arr = [10, 20, 30, 40];
+
+// push() push element in last
+arr.push(30)
+console.log(arr)
+
+// pop() pop element in last
+arr.pop()
+console.log(arr)
+
+// shift() pop element in first
+arr.shift()
+console.log(arr)
+
+// unshift() push element in first
+arr.unshift(10)
+console.log(arr)
+
