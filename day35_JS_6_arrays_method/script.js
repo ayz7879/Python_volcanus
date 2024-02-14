c34ef2c7898d204833a1a828717187a30cfdb154
Ashish <yashish393@gmail.com>
2024-02-14 12:12:03 +0530

    JS_array_methods

---
+++ day35_JS_6_arrays_method/script.js
@@ -0,0 +1,21 @@
+// let arr = [10, 20, 30, 40, 50];
+
+// it will update the orignal array
+// arr.splice(2,2)
+
+// slice() method create a new array
+// console.log(arr.slice(0, 3));
+
+// concat() combine two array in one array
+// let arr1 = ["laptops","Smartphone","Smartwatch","ipad"]
+// let arr2 = ["orange","banana","cherry","mango"]
+// console.log(arr1.concat(arr2))
+
+// indexof() find index of array element 
+// const arr = ["orange","apple","mango","apple"]
+// console.log(arr.indexOf("apple"))
+
+// lastindexof() find index of last element of array
+// let arr = ["orange","Apple","mango","Apple"]
+// console.log(arr.lastIndexOf("Apple"))
+
