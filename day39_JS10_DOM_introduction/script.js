52156e2265d76225da2bee344fb40a4225118bba
Ashish <yashish393@gmail.com>
2024-02-28 13:06:20 +0530

    javascript_DOM

---
+++ day39_JS10_DOM_introduction/script.js
@@ -0,0 +1,42 @@
+// console.log("hello world");
+
+// let element1 = document.getElementsByTagName("h1");
+// let element2 = document.getElementById("h1");
+// let element3 = document.getElementsByClassName("h1");
+
+// console.log(element1)
+// console.log(element2)
+// console.log(element3)
+
+// const element = document.querySelector("h1")
+
+// let heading = document.querySelector("h1");
+// document.querySelector(
+//   ".container"
+// ).innerHTML = `<h1>${heading.innerHTML}</h1>`;
+
+const img = document.querySelector("img");
+
+const changeImg = () => {
+  img.src =
+    "https://cdn.pixabay.com/photo/2023/05/25/18/10/flowers-8017976_640.jpg";
+};
+const changeImg1 = () => {
+  img.src =
+    "https://cdn.pixabay.com/photo/2023/07/22/08/54/european-shorthair-8142959_640.jpg";
+};
+
+// img.src =
+//   "https://cdn.pixabay.com/photo/2023/05/25/18/10/flowers-8017976_640.jpg";
+
+// const changeBgLight = () => {
+//   let body = document.querySelector("body")
+//   body.style.backgroundColor = "white"
+//   body.style.color = "black"
+
+// }
+// const changeBgDark = () => {
+//   let body = document.querySelector("body")
+//   body.style.backgroundColor = "black"
+//   body.style.color = "white"
+// }
