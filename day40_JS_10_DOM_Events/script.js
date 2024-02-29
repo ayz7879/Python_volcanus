816b8290132da47164ec1c82ed0e18f31dc33030
Ashish <yashish393@gmail.com>
2024-02-29 13:12:01 +0530

    javascript

---
+++ day40_JS_10_DOM_Events/script.js
@@ -0,0 +1,27 @@
+// let container = document.querySelector(".container")
+// let items = document.querySelector(".items:nth-child(2)")
+
+// console.log(items.innerText)
+// console.log(items.innerHTML)
+// console.log(items.textContent)
+
+// const items = document.querySelectorAll(".items")
+
+
+// items.forEach((e)=>e.style.backgroundColor = "red")
+
+// document.querySelector("button").addEventListener("click",()=>{
+//     items.forEach((e) => (e.style.backgroundColor = "red"));
+// })
+
+
+// const getInputValue = () => {
+//     let input = document.querySelector("#input")
+//     console.log(input.value)
+// }
+
+let textarea = document.querySelector(".area")
+
+
+
+
